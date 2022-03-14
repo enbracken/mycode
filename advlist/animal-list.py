db6b0791d490116bc33a6903de135a4c0afef726
Elena Bracken <enbracken@users.noreply.github.com>
2022-03-14 22:26:12 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def main():
+
+    animals = ["Fox","Fly","Ant","Bee","Cod","Cat","Dog","Yak","Cow","Hen","Koi","Hog","Jay","Kit"]
+    print(animals[0],animals[1],animals[2],animals[3],animals[4],animals[5],animals[6])
+main()
+
